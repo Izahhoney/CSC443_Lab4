56a8a6e1a3a373e47d2ea560e9e87660319a5c3b
Izahhoney <2020489338@student.uitm.edu.my>
2022-04-27 23:56:43 +0800

    handleform

--- handle_form.php
+++ handle_form.php
@@ -8,8 +8,7 @@
 </head>
 <body>
     <?php #Script handle form
-    echo "<p> Thank you <b>".
-    $_POST ["name"].
+    echo "<p> Thank you, <b>".$_POST ["name"].
     "</b> for the following comments: <br/>".
     $_POST ["comments"]."<p> We will reply to you at <i>". 
     $_POST ["email"]." </i></p>";
