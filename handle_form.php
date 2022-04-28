fe46c903297379c43bcce6494b08962c6039ece9
Izahhoney <2020489338@student.uitm.edu.my>
2022-04-29 05:09:40 +0800

    handleform

--- handle_form.php
+++ handle_form.php
@@ -47,7 +47,7 @@
     }
 
     if ($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["gender"]) {
-echo "Thank you, " .$_POST["name"]. " for the following comments: <br /> <tt>" .
+echo "Thank you, " .$_POST["name"]. " sudi ajar aye: <br /> <tt>" .
 $_POST ["comments"] . " </tt> <p> We will reply to you at <i>" . $_POST["email"].
 "</i></p>";
 echo $message;
